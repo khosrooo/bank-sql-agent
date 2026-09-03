82efe817bde53c300b5ea836301e9b99975be3f3
khosrooo <Hr.khosravani2000@gmail.com>
2026-09-03 19:30:10 +0330

    fix path database

--- src/bank_sql_agent/config.py
+++ src/bank_sql_agent/config.py
@@ -8,4 +8,8 @@ load_dotenv()
 
 # ==================== اتصال دیتابیس و مدل ====================
 
-DB_PATH = "bank_database.db"
+from pathlib import Path
+
+# مسیر ریشه پروژه (یک سطح بالاتر از src)
+ROOT_DIR = Path(__file__).resolve().parent.parent
+DB_PATH = str(ROOT_DIR / "bank_database.db")
